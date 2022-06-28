8bf99fe792174d650702b1e16db900f7e8cd27dc
Farid Sanusi <farid@ugm.ac.id>
2022-06-28 17:18:32 +0700

    locating element by css selector with selenium or playwright

--- 02/elemen-play2.py
+++ 02/elemen-play2.py
@@ -1,6 +1,8 @@
 from playwright.sync_api import sync_playwright
+import time
 
 url_login = 'https://the-internet.herokuapp.com/login'
+url_add_remove_element = 'https://the-internet.herokuapp.com/add_remove_elements/'
 
 with sync_playwright() as pw:
     browser = pw.chromium.launch(headless=False, slow_mo=100)
@@ -9,9 +11,16 @@ with sync_playwright() as pw:
     # by css with class name
     page.wait_for_selector('.radius').click()
     # by css -> tag button and class 
+    time.sleep(1)
     page.wait_for_selector('button.radius').click()
     # by css -> id form and tag button
+    time.sleep(1)
     page.wait_for_selector('#login > button').click()
 
+    # open new page
+    page = browser.new_page()
+    page.goto(url_add_remove_element)
+    # click by css selector
+    page.wait_for_selector('#content > div > button').click()
 
     page.pause()
--- 02/elemen-selenium2.py
+++ 02/elemen-selenium2.py
@@ -3,14 +3,22 @@ from selenium.webdriver.common.by import By
 import time
 
 url_login = 'https://the-internet.herokuapp.com/login'
+url_add_remove_element = 'https://the-internet.herokuapp.com/add_remove_elements/'
 
 driver = webdriver.Chrome()
 
 driver.get(url_login)
-time.sleep(2)
+time.sleep(1)
 # by class name
 driver.find_element(By.CLASS_NAME, 'radius').click()
 # by css -> tag button and class 
+time.sleep(1)
 driver.find_element(By.CSS_SELECTOR, 'button.radius').click()
 # by css -> id form and tag button 
+time.sleep(1)
 driver.find_element(By.CSS_SELECTOR, '#login > button').click()
+
+time.sleep(1)
+driver.get(url_add_remove_element)
+driver.find_element(By.CSS_SELECTOR, '#content > div > button').click()
+
